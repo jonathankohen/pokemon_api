5d136c6168cbdf5a866d3179e45d8c8be2abf5fe
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2020-09-09 14:18:13 -0400

    added axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
     const [pokemon, setPokemon] = useState([]);
     const [isClicked, setIsClicked] = useState(false);
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon')
-            .then((res) => res.json())
-            .then((res) => setPokemon(res.results));
+        axios
+            .get('https://pokeapi.co/api/v2/pokemon')
+            .then((res) => setPokemon(res.data.results));
     }, [isClicked]);
 
     const handleClick = () => {
